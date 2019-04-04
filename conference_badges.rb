12262b6a9c786e9a27e5ba44c9e51c678384e3ec
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-04 22:49:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,9 @@
 # Write your code here.
 def badge_maker(name)
-puts "Hello, my name us #{name}."  
+  puts "Hello, my name us #{name}."
+end
+
+def batch_badge_creator(names)
+  names.each { |badges| puts badge_maker}
+  badges
 end
